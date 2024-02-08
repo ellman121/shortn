b5f7b74407da799ca5e78931e7a49644eae6b462
Elliott Rarden <ellman121@gmail.com>
2024-02-08 17:00:01 +0000

    Tests working for get

--- tests.py
+++ tests.py
@@ -1,23 +1,37 @@
-import unittest
-import mock
-from sqlalchemy import create_mock_engine
-
+from classes import ShortenedURL, UnknownShortcodeError
+from unittest import mock, TestCase, main
+from fastapi import HTTPException
 from url_shortener import get_short_url
 
 
-db_engine = create_mock_engine("", None)
-
-class TestURLShortner(unittest.TestCase):
+class TestURLShortner(TestCase):
     @mock.patch('url_shortener.short_url_store')
-    def test_get_short_url(self, mock_surl_store):
+    def test_get_short_url_success(self, mock_surl_store):
         """
-        Test that we are able to get an existing URL
+        Test `get_short_url` function returns correctly
         """
-        conn = db_engine.connect()
-        get_short_url(conn, "iExist")
+        mock_surl_store.get_url_and_increment_stats.return_value = ShortenedURL(
+            shortcode="iExist",
+            referenced_url="https://some.url"
+        )
 
-        mock_surl_store.get_url_and_increment_stats.assert_called_with(conn, "iExist")
+        resp = get_short_url("iExist")
+        mock_surl_store.get_url_and_increment_stats.assert_called_with("iExist")
+        
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.headers["Location"], "https://some.url")
+    
+    @mock.patch('url_shortener.short_url_store')
+    def test_get_short_url_error(self, mock_surl_store):
+        """
+        Test `get_short_url` function handles unknown codes and raises HTTPException
+        """
+        mock_surl_store.get_url_and_increment_stats.side_effect = UnknownShortcodeError()
+
+        with self.assertRaises(HTTPException):
+            resp = get_short_url("error")
+            mock_surl_store.get_url_and_increment_stats.assert_called_with("error")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
